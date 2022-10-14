0a65800d1b596068aef9e8e291dbe72cbc3d06f5
Safar9808 <safik686@mail.ru>
2022-10-14 18:46:20 +0300

    создал 3 семинар

--- lesson_3/Program.cs
+++ lesson_3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1 type
+
+void Method1()
+{
+    Console.WriteLine("Autor");
+}
+
+Method1();
+
+// 2 type
+
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+Method2(msg: "sometext", count: 4); // или Method2('some text', 4)
+
