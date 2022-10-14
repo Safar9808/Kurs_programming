f7d4e3b82fb2e3aa6e94f1fba4d46cfd433bcbc7
Safar9808 <safik686@mail.ru>
2022-10-14 14:56:09 +0300

    Add code

--- lesson_2/Program.cs
+++ lesson_2/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ /*int Max(int arg1, int arg2, int arg3)
+ {
+    int result = arg1;
+    if( arg2>result) result=arg2;
+    if( arg3>result) result=arg3;
+    return result;
+ }
+
+int a1=3;
+int a2=55;
+int a3=4;
+int a4=6;
+int a5=2;
+int a6=7;
+int a7=75;
+int a8=4;
+int a9=1;
+
+int max1=Max(a1,a2,a3);
+int max2=Max(a4,a5,a6);
+int max3=Max(a7,a8,a9);
+
+int max=Max(max1,max2,max3);
+Console.WriteLine(max);
+*/
+
+/*
+int[] array = {11,21,31,41,15,61,17,18};
+array[0]=12;
+Console.WriteLine(array[4]);
+*/
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index<length)
+    {
+        collection[index]= new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1>number2)
+{
+    Console.WriteLine("Большее из чисел: "+number1);
+}
+else
+{
+    Console.WriteLine("Большее из чисел: "+number2);
+}
+*/
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1>number2)
+{
+    Console.WriteLine("Большее из чисел: "+number1);
+    Console.WriteLine("Меньшее из чисел: "+number1);
+}
+else
+{
+    Console.WriteLine("Большее из чисел: "+number2);
+    Console.WriteLine("Меньшее из чисел: "+number1);
+}
