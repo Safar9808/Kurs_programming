82f30e5d53b342fb55edf02b76366db91742c73d
Safar9808 <safik686@mail.ru>
2022-10-15 22:57:00 +0300

    обновил код + тернарный оператор

--- seminar_3/Program.cs
+++ seminar_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход координаты точки (X и Y),
+   причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка */
+
+string chetvert(int x, int y)
+{
+    string name = x>0 && y>0 ? "1 четверть" : x<0 && y>0 ? "2 четверть" : x<0 && y<0 ? "3 четверть" : "4 четверть"; // тернартный оператор
+    return name;
+}
+Console.Write("Enter x: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Enter y: ");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine(chetvert(x,y));
+
