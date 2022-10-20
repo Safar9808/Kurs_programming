c2c60b75c2bbc699c232720dddf77208e4bcfe7d
Safar9808 <safik686@mail.ru>
2022-10-20 15:13:18 +0300

    решил 1 задачу

--- seminar_5/Program.cs
+++ seminar_5/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  ДОМАШНИЕ ЗАДАНИЯ:
+    Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+    Напишите программу, которая покажет количество чётных чисел в массиве. */
+int WriteRead(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void vivod(int[] arr)
+{
+    foreach(var i in arr)
+    {
+        Console.Write(i+" ");
+    }
+    Console.WriteLine();
+}
+int[] CreateFillArr(int d)
+{
+    int[] arr = new int[d];
+    for (int i = 0; i < d; i++) arr[i]= new Random().Next(100,199);
+    return arr;
+}
+int NumEven(int[] arr)
+{
+    int k=0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]%2==0) k++;
+    }
+    return k;
+}
+
+int A = WriteRead("Введите размер массива: ");
+int[] aaa = CreateFillArr(A);
+vivod(aaa);
+Console.WriteLine($"Количество четных чисел: {NumEven(aaa)}");
+
