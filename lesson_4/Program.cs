d14e896f2dd1f15ca801bb57901125648df6fc5e
Safar9808 <safik686@mail.ru>
2022-10-21 13:10:58 +0300

    Ввели двумерные массивы

--- lesson_4/Program.cs
+++ lesson_4/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[,] table = new string[2,5];                                      // изначально String.Empty
+// table [0,0] table [0,1] table [0,2] table [0,3] table [0,4] 
+// table [1,0] table [1,1] table [1,2] table [1,3] table [1,4] 
+table[1,2] = "слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.Write($"-{table[rows, columns]}- ");
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)                        
+    {
+        for (int g = 0; g < matr.GetLength(1); g++)                   
+        {
+            Console.Write($"{matr[i, g]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)                        
+    {
+        for (int g = 0; g < matr.GetLength(1); g++)                   
+        {
+            matr[i,g]=new Random().Next(1,10);  //[1,10) полуинтервал
+        }
+    }
+    Console.WriteLine();
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+PrintArray(matrix);
