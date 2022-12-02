c2c1ae13af3e8211107e06c8bfc5b0dbf1bdc82f
Safar9808 <safik686@mail.ru>
2022-12-02 18:03:21 +0400

    начинаю

--- lesson_7/Program.cs
+++ lesson_7/Program.cs
@@ -1,18 +1,1 @@
-﻿//буду вспоминать что происходит 
-
-int factorialchik (int n)
-{
-    if (n==1) return 1;
-    else return n*factorialchik(n-1);
-}
-
-int WriteRead(string msg)
-{
-    Console.Write(msg);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-int N = WriteRead("введи число n: ");
-Console.WriteLine(factorialchik(N));
-
-// добавил комент для пуша
+﻿Console.WriteLine("ger");
